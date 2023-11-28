8be1b333b5ad3c148cd300dcf0f141a07592e0d5
Egle Juraityte <lokajata@gmail.com>
2023-11-28 16:41:15 +0200

    skeleton

---
+++ src/main/java/Secret/Santa/Secret/Santa/SecretSantaApplication.java
@@ -0,0 +1,17 @@
+package Secret.Santa.Secret.Santa;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Secret Santa API", version = "1.0",
+		description = "Secret Santa API"))
+public class SecretSantaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretSantaApplication.class, args);
+	}
+
+}
