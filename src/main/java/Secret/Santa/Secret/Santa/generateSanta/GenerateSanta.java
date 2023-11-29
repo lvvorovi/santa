9110ae833a90dafa10aaeea210c40c90d75b0cae
Egle Juraityte <lokajata@gmail.com>
2023-11-29 11:03:11 +0200

    Creat base

---
+++ src/main/java/Secret/Santa/Secret/Santa/generateSanta/GenerateSanta.java
@@ -0,0 +1,11 @@
+package Secret.Santa.Secret.Santa.generateSanta;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenerateSanta {
+}
---
+++ src/main/java/Secret/Santa/Secret/Santa/users/Users.java
@@ -0,0 +1,11 @@
+package Secret.Santa.Secret.Santa.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+}
