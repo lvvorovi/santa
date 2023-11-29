be852c8a2b59f38386b3945fc600e37ab9a30b24
Sakivan <benas.navikas@ktu.edu>
2023-11-29 15:07:16 +0200

    added controllers

--- src/main/java/Secret/Santa/Secret/Santa/controllers/UserController.java
+++ src/main/java/Secret/Santa/Secret/Santa/controllers/UserController.java
@@ -2,20 +2,60 @@ package Secret.Santa.Secret.Santa.controllers;
 
 import Secret.Santa.Secret.Santa.models.DTO.UserDTO;
 import Secret.Santa.Secret.Santa.models.User;
+import Secret.Santa.Secret.Santa.repos.IUserRepo;
+import Secret.Santa.Secret.Santa.services.IUserService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-    @PostMapping
-    public ResponseEntity<User> createLessor(@RequestBody @Valid UserDTO userDTO, BindingResult bindingResult) {
-        return null;
+    @Autowired
+    private IUserRepo iUserRepo;
+    @Autowired
+    private IUserService iUserService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = iUserService.getAllUsers();
+        return ResponseEntity.ok(users);
     }
+    @PostMapping
+    public ResponseEntity<User> createLessor(@RequestBody @Valid UserDTO userDTO) {
+        User user = iUserService.createUser(userDTO);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("{userid}")
+    public ResponseEntity<User> getUserById(@Valid
+                                                @Min(value = 1, message = "ID must be a non-negative integer and greater than 0")
+                                                @PathVariable int userid){
+        User user = null;
+        user = iUserService.findByUserid(userid);
+        return ResponseEntity.ok(user);
+    }
+    @PutMapping("{userid}")
+    public ResponseEntity<User> updateUser(@Valid
+                                               @Min(value = 1, message = "ID must be a non-negative integer and greater than 0")
+                                               @PathVariable int userid,@RequestBody @Valid UserDTO userDTO) {
+        User user = iUserService.editByUserId(userDTO, userid);
+        return ResponseEntity.ok(user);
+    }
+    @DeleteMapping("{userid}")
+    public ResponseEntity<String> deleteUser(@Valid @Min(value = 1, message = "ID must be a non-negative integer and greater than 0")
+                                               @PathVariable int userid) {
+        if (Boolean.TRUE.equals(iUserService.deleteUserByUserid(userid))) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.badRequest().body("Delete failed");
+    }
+
 }
