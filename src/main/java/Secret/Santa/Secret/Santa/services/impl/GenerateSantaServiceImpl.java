a0262c31ae572dc6b3ae006467fbddff2fac5b73
reinisrm <rreeiinniiss@gmail.com>
2023-12-05 12:34:07 +0200

    Added error logging to GenerateSantaServiceImpl

--- src/main/java/Secret/Santa/Secret/Santa/services/impl/GenerateSantaServiceImpl.java
+++ src/main/java/Secret/Santa/Secret/Santa/services/impl/GenerateSantaServiceImpl.java
@@ -11,6 +11,8 @@ import Secret.Santa.Secret.Santa.validationUnits.GroupUtils;
 import Secret.Santa.Secret.Santa.validationUnits.UserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -18,6 +20,7 @@ import static Secret.Santa.Secret.Santa.mappers.GenerateSantaMapper.toSanta;
 
 @Service
 public class GenerateSantaServiceImpl implements IGenerateSantaService {
+    private static final Logger logger = LoggerFactory.getLogger(GenerateSantaServiceImpl.class);
 
     @Autowired
     private IGenerateSantaRepo generateSantaRepository;
@@ -37,46 +40,71 @@ public class GenerateSantaServiceImpl implements IGenerateSantaService {
 
     @Override
     public List<GenerateSanta> getAllGenerateSantaByGroup(Integer groupId) {
-        Group group = groupUtils.getGroupById(groupId);//groupRepository.findById(groupId).orElseThrow();
-        return generateSantaRepository.findByGroup(group);
+        try {
+            Group group = groupUtils.getGroupById(groupId);//groupRepository.findById(groupId).orElseThrow();
+            return generateSantaRepository.findByGroup(group);
+        } catch (Exception e) {
+            logger.error("Error retrieving all GenerateSanta by group ID: {}", groupId, e);
+            throw e;
+        }
     }
 
     @Override
     public GenerateSanta createGenerateSanta(GenerateSantaDTO generateSantaDTO) {
-
-        return generateSantaRepository.save(toSanta(generateSantaDTO));
+        try {
+            return generateSantaRepository.save(toSanta(generateSantaDTO));
+        } catch (Exception e) {
+            logger.error("Error creating GenerateSanta", e);
+            throw e;
+        }
     }
 
     @Override
     public GenerateSanta getGenerateSantaBySantaAndGroup(Integer santaId, Integer groupId) {
-
-        User santa = userUtils.getUserById(santaId);
-        Group group = groupUtils.getGroupById(groupId);
-
-        return generateSantaUtils.getBySantaAndGroup(santa, group);
+        try {
+            User santa = userUtils.getUserById(santaId);
+            Group group = groupUtils.getGroupById(groupId);
+            return generateSantaUtils.getBySantaAndGroup(santa, group);
+        } catch (Exception e) {
+            logger.error("Error retrieving GenerateSanta by santa ID: {} and group ID: {}", santaId, groupId, e);
+            throw e;
+        }
     }
 
     @Override
     public void deleteGenerateSantaById(Integer id) {
-        generateSantaRepository.deleteById(id);
+        try {
+            generateSantaRepository.deleteById(id);
+        } catch (Exception e) {
+            logger.error("Error deleting GenerateSanta by ID: {}", id, e);
+            throw e;
+        }
     }
 
     @Override
     public void deleteGenerateSantaByGroup(Integer groupId) {
-
-        Group group = groupUtils.getGroupById(groupId);
-        generateSantaRepository.deleteByGroup(group);
+        try {
+            Group group = groupUtils.getGroupById(groupId);
+            generateSantaRepository.deleteByGroup(group);
+        } catch (Exception e) {
+            logger.error("Error deleting GenerateSanta by group ID: {}", groupId, e);
+            throw e;
+        }
     }
 
     @Override
     public void deleteGenerateSantaByUser(Integer userId, Integer groupId) {
-        Group group = groupUtils.getGroupById(groupId);
-        User user = userUtils.getUserById(userId);
-        GenerateSanta generateSanta = generateSantaUtils.getBySantaAndGroup(user, group);//generateSantaRepository.findBySantaAndGroup(user, group);
-        GenerateSanta generateSantaRecipient = generateSantaUtils.getByUserAndGroup(user, group);//generateSantaRepository.findByUserAndGroup(user, group);
-
-        generateSantaRecipient.setRecipient(generateSanta.getRecipient());
-        generateSantaRepository.delete(generateSanta);
+        try {
+            Group group = groupUtils.getGroupById(groupId);
+            User user = userUtils.getUserById(userId);
+            GenerateSanta generateSanta = generateSantaUtils.getBySantaAndGroup(user, group);
+            GenerateSanta generateSantaRecipient = generateSantaUtils.getByUserAndGroup(user, group);
+            generateSantaRecipient.setRecipient(generateSanta.getRecipient());
+            generateSantaRepository.delete(generateSanta);
+        } catch (Exception e) {
+            logger.error("Error deleting GenerateSanta by user ID: {} and group ID: {}", userId, groupId, e);
+            throw e;
+        }
     }
 //@Override
 //        public void randomSantaGenerator(Group group) {
