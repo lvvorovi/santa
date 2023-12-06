b67fb3f1a0c50f32368ae7b9c0eb3fac3d4f9321
Ingrida <ingridarrr@gmail.com>
2023-12-06 09:27:24 +0200

    dto updates

--- src/main/java/Secret/Santa/Secret/Santa/models/DTO/GroupDTO.java
+++ src/main/java/Secret/Santa/Secret/Santa/models/DTO/GroupDTO.java
@@ -3,6 +3,7 @@ package Secret.Santa.Secret.Santa.models.DTO;
 import Secret.Santa.Secret.Santa.models.GenerateSanta;
 import Secret.Santa.Secret.Santa.models.Gift;
 import Secret.Santa.Secret.Santa.models.User;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,8 +19,11 @@ import java.util.List;
 @NoArgsConstructor
 public class GroupDTO {
     private int groupId;
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotBlank(message = "Event date is required")
     private LocalDate eventDate;
+    @NotBlank(message = "Budget is required")
     private double budget;
     private List<User> user;
     private List<Gift> gifts;
