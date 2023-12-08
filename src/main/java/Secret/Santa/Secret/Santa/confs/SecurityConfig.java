fb18c6173d3537c67d9074b04cec7f8fb6a5ee17
Sakivan <benas.navikas@ktu.edu>
2023-12-08 11:54:41 +0200

    Changes after merge. Fixed SecurityConfig.

--- src/main/java/Secret/Santa/Secret/Santa/confs/SecurityConfig.java
+++ src/main/java/Secret/Santa/Secret/Santa/confs/SecurityConfig.java
@@ -29,9 +29,9 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfig {
 
 
-    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**",
-                                                    "/swagger-ui/",
-                                                    "/v3/api-docs/",
+    private static final String[] WHITE_LIST = {"/api/v1/auth/**",
+                                                    "/swagger-ui/**",
+                                                    "/v3/api-docs/**",
                                                     "/v3/api-docs.yaml",
                                                     "/swagger-ui.html"};
     private final JwtAuthenticationFilter jwtAuthFilter;
@@ -75,7 +75,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(req ->
                         req
-                                .requestMatchers("/api/v1/auth/**")
+                                .requestMatchers(WHITE_LIST)
                                 .permitAll()
 //                                .requestMatchers(HttpMethod.GET, "/api/v1/groups/**").authenticated()
 //                                .requestMatchers(HttpMethod.POST, "/api/v1/groups/**").authenticated()
