fedb7a383e68de7dc18db151089fc3b171da2aed
Ingrida <ingridarrr@gmail.com>
2023-12-09 20:05:56 +0200

    navbar login/logout updated

--- santaprojectfrontend/src/components/Navbar.js
+++ santaprojectfrontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { React, useContext } from "react";
 import { Segment, Menu, Button } from "semantic-ui-react";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AuthContext from "../AuthContext";
 import { NavLink } from "react-router-dom";
 
@@ -8,16 +8,7 @@ export function Navbar() {
   const { appState, setAppState } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  // const logoutHandler = async () => {
-  //   fetch("/logout", {
-  //     method: "POST",
-  //   })
-  //     .then(localStorage.clear)
-  //     .then((response) => {
-  //       setAppState({ type: "LOGOUT" });
-  //       navigate("/", { replace: true });
-  //     });
-  // };
+ 
   const logoutHandler = () => {
     console.log("LOG OUT");
     localStorage.clear();
@@ -36,25 +27,25 @@ export function Navbar() {
           name="home"
           content="SECRET SANTA"
         />
-        {/* )}      
-          {!appState.isAuthenticated && ( */}
-        <Menu.Item
-          position="right"
-          as={NavLink}
-          exact
-          to="/auth/login"
-          name="login"
-          content="Log in"
-        />
-        {/* )}
-        {appState.isAuthenticated && ( */}
-        <Menu.Item
-          position="right"
-          as={Button}
-          onClick={logoutHandler}
-          name="logout"
-          content="Log out"
-        />
+        {!appState.isAuthenticated && (
+          <Menu.Item
+            position="right"
+            as={NavLink}
+            exact
+            to="/auth/login"
+            name="login"
+            content="Log in"
+          />
+        )}
+        {appState.isAuthenticated && (
+          <Menu.Item
+            position="right"
+            as={Button}
+            onClick={logoutHandler}
+            name="logout"
+            content="Log out"
+          />
+        )}
       </Menu>
     </Segment>
   );
